2a7607b40b28e5f51bfc23a15ba8e2d617e498cb
gbinder <admin@groovebind.com>
2014-03-23 20:18:56 -0700

    generated via buildClientDm.sh; the previous one was generated from json2pojo tool

--- src/models/ddm/User.java
+++ src/models/ddm/User.java
@@ -1,47 +1,75 @@
+
 package models.ddm;
 
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.Generated;
+/** 
+ * Schema fragment(s) for this class:
+ * <pre>
+ * &lt;xs:complexType xmlns:ns="http://ddm.models" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="User">
+ *   &lt;xs:sequence>
+ *     &lt;xs:element type="xs:string" name="firstName"/>
+ *     &lt;xs:element type="xs:string" name="surName" minOccurs="0"/>
+ *   &lt;/xs:sequence>
+ *   &lt;xs:attribute type="xs:long" name="Id"/>
+ * &lt;/xs:complexType>
+ * </pre>
+ */
+public class User
+{
+    private String firstName;
+    private String surName;
+    private Long id;
 
-@Generated("org.jsonschema2pojo")
-public class User {
+    /** 
+     * Get the 'firstName' element value.
+     * 
+     * @return value
+     */
+    public String getFirstName() {
+        return firstName;
+    }
 
-private String firstName;
-private String surName;
-private Integer id;
-private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    /** 
+     * Set the 'firstName' element value.
+     * 
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-public String getFirstName() {
-return firstName;
-}
+    /** 
+     * Get the 'surName' element value.
+     * 
+     * @return value
+     */
+    public String getSurName() {
+        return surName;
+    }
 
-public void setFirstName(String firstName) {
-this.firstName = firstName;
-}
+    /** 
+     * Set the 'surName' element value.
+     * 
+     * @param surName
+     */
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 
-public String getSurName() {
-return surName;
-}
+    /** 
+     * Get the 'Id' attribute value.
+     * 
+     * @return value
+     */
+    public Long getId() {
+        return id;
+    }
 
-public void setSurName(String surName) {
-this.surName = surName;
-}
-
-public Integer getId() {
-return id;
-}
-
-public void setId(Integer id) {
-this.id = id;
-}
-
-public Map<String, Object> getAdditionalProperties() {
-return this.additionalProperties;
-}
-
-public void setAdditionalProperty(String name, Object value) {
-this.additionalProperties.put(name, value);
-}
-
+    /** 
+     * Set the 'Id' attribute value.
+     * 
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
